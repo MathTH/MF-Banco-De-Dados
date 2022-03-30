41fa74675dc8cf7564e7a735fb1402f2a78be3aa
MathTH <mathdhferreira@gmail.com>
2022-03-30 14:34:34 -0300

    Editando os espaços

--- mf_puxando_dados_de_outro_bd.sql
+++ mf_puxando_dados_de_outro_bd.sql
@@ -4,3 +4,4 @@ INSERT INTO contatos (codigo_id, nome) SELECT codigo_id, nome FROM cadastro_new 
 //Puxando o 'ID', 'NOME' eo 'E-MAIL' da tabela 'contatos' para passar para a tabela 'e_mail'
 INSERT INTO e_mail (codigo_id, nome, e_mail) SELECT codigo_id, nome, e_mail FROM contatos WHERE (codigo_id LIKE '1') GROUP BY codigo_id;
 INSERT INTO e_mail (codigo_id, nome, e_mail) SELECT codigo_id, nome, e_mail FROM contatos WHERE (codigo_id LIKE '2') GROUP BY codigo_id;
+
